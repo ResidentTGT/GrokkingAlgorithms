3cdb0de3b4664d7fe3c1425180a2c72f118ff16b
Anatoly Romanov <tromanov95@mail.ru>
2021-06-11 21:43:05 +0300

    finished greedy algorithm

--- src/scripts/greedy.js
+++ src/scripts/greedy.js
@@ -9,7 +9,9 @@ greedyTemplate.innerHTML = `
     In many problems, a greedy strategy does not usually produce an optimal solution, but nonetheless, a greedy heuristic may yield locally optimal solutions that approximate a globally optimal solution in a reasonable amount of time.
     </div>
     <div class="example-title">Example</div>
+    <div class="condition">We want to find the minimum set of stations that covers all states: mt, wa, or, id, nv, ut.</div>
     <button class="action-button generate">Generate stations!</button>
+    <div class="condition stations"></div>
     <button class="action-button find" disabled>Find the !</button>
     <div class="result"></div>
     `;
@@ -18,15 +20,14 @@ class GreedyComponent extends HTMLElement {
     constructor() {
         super();
 
-        this._states = new Set(['mt', 'wa', 'or', 'id', 'nv', 'ut']);
-        this._stations = new Map([['one'], ['two'], ['three'], ['four'], ['five']]);
+        this.initStatesAndStations();
 
         this._shadowRoot = this.attachShadow({ mode: 'open' });
         this._shadowRoot.appendChild(greedyTemplate.content.cloneNode(true));
 
         this.$generateButton = this.shadowRoot.querySelector('.action-button.generate');
         this.$findButton = this.shadowRoot.querySelector('.action-button.find');
-        this.$graph = this.shadowRoot.querySelector('.graph');
+        this.$stations = this.shadowRoot.querySelector('.stations');
         this.$result = this.shadowRoot.querySelector('.result');
     }
 
@@ -40,15 +41,63 @@ class GreedyComponent extends HTMLElement {
         this.shadowRoot.querySelector('.action-button.find').removeEventListener();
     }
 
+    initStatesAndStations() {
+        this._states = new Set(['mt', 'wa', 'or', 'id', 'nv', 'ut']);
+        this._stations = new Map([['one'], ['two'], ['three'], ['four'], ['five']]);
+    }
+
     generateStationsClickHandler() {
-        const allStates = Array.from(this._states);
+        this.initStatesAndStations();
+
+        const allStates = [...this._states];
+
+        const statesStations = new Set();
         for (const [station] of this._stations) {
-            const states = [];
+            const states = new Set();
             for (let i = 0; i < 3; i++) {
-                states.push(allStates[generateRandomInteger(0, 5)]);
+                const randomState = allStates[window.generateRandomInteger(0, allStates.length - 1)];
+                states.add(randomState);
+                statesStations.add(randomState);
             }
             this._stations.set(station, states);
         }
+        const remainedStates = allStates.filter(s => !statesStations.has(s));
+
+        this._stations.set('one', new Set([...this._stations.get('one')].concat(remainedStates)));
+
+        this.showStations();
+
+        this.$findButton.removeAttribute('disabled');
+    }
+
+    dijkstraClickHandler() {
+        const finalStations = new Set();
+        let statesNeeded = new Set([...this._states]);
+
+        while (statesNeeded.size) {
+            let bestStation;
+            let statesCovered = new Set();
+            for (const [station, states] of this._stations) {
+                const covered = new Set([...statesNeeded].filter(x => states.has(x)));
+                if (covered.size > statesCovered.size) {
+                    bestStation = station;
+                    statesCovered = covered;
+                }
+            }
+            statesNeeded = new Set([...statesNeeded].filter(x => !statesCovered.has(x)));
+            finalStations.add(bestStation);
+        }
+
+        this.$result.innerHTML = `Final stations are: ${[...finalStations].join(', ')}`;
+    }
+
+    showStations() {
+        let str = '';
+        for (const [station, states] of this._stations) {
+            str += `Station ${station}: ${[...states].join(', ')}<br>`;
+        }
+        this.$stations.innerHTML = str;
+        this.$result.innerHTML = '';
     }
 }
 
