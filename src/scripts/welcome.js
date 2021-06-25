1f67701fc81a6ff804564ae7da218d2a5e584b51
Anatoly Romanov <tromanov95@mail.ru>
2021-06-25 16:23:47 +0300

    added decorations, refs to algorithms, changed scroll in chrome

--- src/scripts/welcome.js
+++ src/scripts/welcome.js
@@ -6,8 +6,7 @@ welcomeTemplate.innerHTML = `
         <img src="assets/book.jpg" />
         <div class="description">
             <span class="welcome">Welcome!</span><br>
-            This project is dedicated to the book <a href="https://www.amazon.com/Grokking-Algorithms-illustrated-programmers-curious/dp/1617292230" target="blank">Grokking Algorithms by Aditya Y. Bhargava</a>, describes and
-            implements algorithms from the book. Each presented example in the book includes code samples in
+            This project is dedicated to the book <a href="https://www.amazon.com/Grokking-Algorithms-illustrated-programmers-curious/dp/1617292230" target="blank">Grokking Algorithms by Aditya Y. Bhargava</a>. Each presented example in the book includes code samples in
             Python. In this project all algorithms were implemented in pure JavaScript.<br />
             Each section includes short description of algorithm, dynamic example (you dynamically generate
             input parameters), result of the algorithm and link to implementation in GitHub.<br />
