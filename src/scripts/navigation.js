7f4374b22e4808c301c899ce386a4f1bbaaaa1ce
Anatoly Romanov <tromanov95@mail.ru>
2021-07-06 09:43:12 +0300

    scroll in progress

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -4,13 +4,28 @@ window.addEventListener('DOMContentLoaded', () => {
         colorLinks(getLinks());
     }
 
-    addAlgorithmsScrollEventListener();
+    addLiClickListener(getLinks(), getSections());
+
+    addAlgorithmsScrollEventListener(getSections());
     addMenuEventListener();
 });
 
 window.addEventListener('popstate', () => {
     colorLinks(getLinks());
 });
+
+function addLiClickListener(links, sections) {
+    const mainBlock = document.body.querySelector('.algorithms');
+
+    links.forEach(li => {
+        li.addEventListener('click', e => {
+            const name = e.target.attributes.getNamedItem('name').value;
+            location.hash = name;
+            const activeSection = sections.find(s => `${s.attributes.getNamedItem('name').value}` === name);
+            mainBlock.scrollTo({ top: activeSection.offsetTop - mainBlock.offsetTop });
+        });
+    });
+}
 
 function addMenuEventListener() {
     const menu = document.querySelector('button.menu');
@@ -22,16 +37,16 @@ function addMenuEventListener() {
 }
 
 function getLinks() {
-    return Array.from(document.querySelectorAll('aside > a'));
+    return Array.from(document.querySelectorAll('aside ul li'));
 }
 
 function getSections() {
-    return Array.from(document.querySelectorAll('.algorithm-layout > a'));
+    return Array.from(document.querySelectorAll('.algorithm-layout'));
 }
 
 function colorLinks(links) {
     links.forEach(l => {
-        if (l.attributes.getNamedItem('href').value === location.hash) {
+        if (`#${l.attributes.getNamedItem('name').value}` === location.hash) {
             l.classList.add('active');
         } else {
             l.classList.remove('active');
@@ -40,24 +55,21 @@ function colorLinks(links) {
 }
 
 function moveToActiveSection(sections) {
-    const activeSection = sections.find(s => `#${s.name}` === location.hash);
+    const activeSection = sections.find(s => `#${s.attributes.getNamedItem('name').value}` === location.hash);
+    const mainBlock = document.body.querySelector('.algorithms');
 
     if (activeSection) {
-        setTimeout(() => document.body.querySelector('.algorithms').scrollTo({ top: activeSection.offsetTop }), 0);
+        setTimeout(() => mainBlock.scrollTo({ top: activeSection.offsetTop - mainBlock.offsetTop }), 0);
     }
 }
 
-function addAlgorithmsScrollEventListener() {
+function addAlgorithmsScrollEventListener(sections) {
     document.body.querySelector('.algorithms').addEventListener('scroll', e => {
-        setTimeout(() => {
-            const sections = getSections();
-            const activeSection = sections
-                .filter(s => e.target.scrollTop + e.target.offsetTop + 1 > s.offsetTop)
-                .reverse()[0];
-            if (activeSection && e.target.scrollTop + e.target.offsetHeight + 1 < e.target.scrollHeight) {
-                history.replaceState(null, null, document.location.pathname + '#' + activeSection.name);
-                colorLinks(getLinks());
-            }
-        }, 1000);
+        const activeSection = sections
+            .filter(s => e.target.scrollTop + e.target.offsetTop + 1 > s.offsetTop)
+            .reverse()[0];
+        if (activeSection && e.target.scrollTop + e.target.offsetHeight + 1 < e.target.scrollHeight) {
+            location.hash = activeSection.attributes.getNamedItem('name').value;
+        }
     });
 }
