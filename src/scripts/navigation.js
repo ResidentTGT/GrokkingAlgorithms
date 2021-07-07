f4693d68cbf8e2a208911b2b007587eafd2d7b49
Anatoly Romanov <tromanov95@mail.ru>
2021-07-07 13:31:22 +0300

    added closing aside in mobile version when click outside and navigation

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -1,6 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
+    const asideElement = document.querySelector('aside');
     const mainBlock = document.querySelector('.algorithms');
     const sections = Array.from(document.querySelectorAll('.algorithm-layout'));
+    const menuButton = document.querySelector('header .menu');
+    const titleElement = document.querySelector('header .title');
     const links = getLinks();
 
     if (location.hash) {
@@ -8,32 +11,50 @@ window.addEventListener('DOMContentLoaded', () => {
         colorLinks(links);
     }
 
-    addLiClickListener(links, sections, mainBlock);
+    addTitleClickEventListener(titleElement, mainBlock);
+    addLiClickEventListener(links, sections, mainBlock, menuButton, asideElement);
+    addAlgorithmsClickEventListener(mainBlock, menuButton, asideElement);
     addAlgorithmsScrollEventListener(sections, mainBlock);
-    addMenuEventListener();
+    addMenuClickEventListener(menuButton, asideElement);
 });
 
 window.addEventListener('popstate', () => {
     colorLinks(getLinks());
 });
 
+function addTitleClickEventListener(titleElement, mainBlock) {
+    titleElement.addEventListener('click', () => scrollTo(mainBlock));
+}
+
 function moveToActiveSection(sections, mainBlock) {
     const activeSection = findActiveBlock(sections, location.hash.slice(1));
 
     if (activeSection) {
-        setTimeout(() => scrollTo(activeSection, mainBlock), 500);
+        setTimeout(() => scrollTo(mainBlock, activeSection), 500);
     }
 }
 
-function addLiClickListener(links, sections, mainBlock) {
+function addLiClickEventListener(links, sections, mainBlock, menuButton, asideElement) {
     links.forEach(li => {
         li.addEventListener('click', e => {
             const name = e.target.attributes.getNamedItem('name').value;
             location.hash = name;
 
             const activeSection = findActiveBlock(sections, name);
-            scrollTo(activeSection, mainBlock);
+            scrollTo(mainBlock, activeSection);
+
+            if (getComputedStyle(menuButton).display !== 'none') {
+                showAside(asideElement);
+            }
         });
+    });
+}
+
+function addAlgorithmsClickEventListener(mainBlock, menuButton, asideElement) {
+    mainBlock.addEventListener('click', () => {
+        if (getComputedStyle(menuButton).display !== 'none' && getComputedStyle(asideElement).display !== 'none') {
+            showAside(asideElement);
+        }
     });
 }
 
@@ -49,13 +70,12 @@ function addAlgorithmsScrollEventListener(sections, mainBlock) {
     });
 }
 
-function addMenuEventListener() {
-    const menu = document.querySelector('button.menu');
+function addMenuClickEventListener(menuButton, asideElement) {
+    menuButton.addEventListener('click', () => showAside(asideElement));
+}
 
-    menu.addEventListener('click', () => {
-        const aside = document.querySelector('aside');
-        aside.style.display = aside.style.display === 'block' ? 'none' : 'block';
-    });
+function showAside(asideElement) {
+    asideElement.style.display = getComputedStyle(asideElement).display === 'none' ? 'block' : 'none';
 }
 
 function getLinks() {
@@ -66,8 +86,9 @@ function findActiveBlock(sections, name) {
     return sections.find(s => `${s.attributes.getNamedItem('name').value}` === name);
 }
 
-function scrollTo(activeSection, mainBlock) {
-    mainBlock.scrollTo({ top: activeSection.offsetTop - mainBlock.offsetTop });
+function scrollTo(mainBlock, activeSection) {
+    const offset = activeSection ? activeSection.offsetTop - mainBlock.offsetTop : 0;
+    mainBlock.scrollTo({ top: offset });
 }
 
 function colorLinks(links) {
